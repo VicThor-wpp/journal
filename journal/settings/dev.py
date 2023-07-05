2af4db879035bcc4a10c9735c591d9665bbf4877
VicThor <victor.aragon@wmlyr.com>
2023-07-05 15:12:38 -0300

    test1

---
+++ journal/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-wlli@@&r=)%trb^1*s+9)kfs6c@9nwq$)u6r^e2u@-ykdt@mv6"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ research/models.py
@@ -0,0 +1,114 @@
+from django import forms
+from django.db import models
+
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+from wagtail import blocks
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField, StreamField
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel, FieldRowPanel
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.search import index
+
+from wagtailcodeblock.blocks import CodeBlock
+from wagtail.snippets.models import register_snippet
+
+class ResearchTagIndexPage(Page):
+    def get_context(self, request):
+        # Filter by tag
+        tag = request.GET.get('tag')
+        researchpages = ResearchPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['researchpages'] = researchpages
+        return context
+
+class ResearchIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        researchpages = self.get_children().live().order_by('-first_published_at')
+        context['researchpages'] = researchpages
+        return context
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
+
+class ResearchPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'ResearchPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
+class ResearchPage(Page):
+    date = models.DateField("Post date")
+    abstract = models.TextField()
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="title")),
+        ('paragraph', blocks.TextBlock()),
+        ('image', ImageChooserBlock()),
+        ("code", CodeBlock(label='Code')),
+    ], use_json_field=True)
+    tags = ClusterTaggableManager(through=ResearchPageTag, blank=True)
+    categories = ParentalManyToManyField('research.ResearchCategory', blank=True)
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+        
+    search_fields = Page.search_fields + [
+        index.SearchField('abstract'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('date'),
+            FieldPanel('tags'),
+            FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
+        ], heading="Research information"),
+        FieldPanel('abstract'),
+        FieldPanel('body'),
+        InlinePanel('gallery_images', label="Gallery images"),
+    ]
+
+class ResearchPageGalleryImage(Orderable):
+    page = ParentalKey(ResearchPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('caption'),
+    ]
+
+@register_snippet
+class ResearchCategory(models.Model):
+    name = models.CharField(max_length=255)
+    icon = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('icon'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'research categories'
